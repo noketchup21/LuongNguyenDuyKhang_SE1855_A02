6dd8ef03b245e3a037bc6da2a0c0318329c27c24
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-07-19 15:01:23 +0700

    add appsettings json and connect db from it

--- DataAccessLayer/LucySalesDataContext.cs
+++ DataAccessLayer/LucySalesDataContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer;
 
@@ -29,9 +30,19 @@ public partial class LucySalesDataContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:DBContext"];
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-JUBLI2B6\\SQLEXPRESS;Database=Lucy_SalesData;uid=SA;pwd=12345;TrustServerCertificate=True;");
+    {
+        string strConnection = GetConnectionString();
+        optionsBuilder.UseSqlServer(strConnection);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
